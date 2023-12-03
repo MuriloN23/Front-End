01852963bc0e1af5af4951f0ffa5ecc784212bf0
Murilo Novais <muhnovais@gmail.com>
2023-12-03 16:37:33 -0300

    Commit atualização

---
+++ IP-004/script.js
@@ -0,0 +1,18 @@
+function capturarInformacoes(botao) {
+    var elemento = botao.parentElement;
+    var cidade = elemento.querySelector('h3').textContent;
+    var preco = elemento.querySelector('h2').textContent;
+    var detalhes = elemento.querySelectorAll('ul li');
+
+    var pacoteTuristico = {
+        cidade: cidade,
+        preco: preco,
+        detalhes: []
+    };
+
+    detalhes.forEach(function (item) {
+        pacoteTuristico.detalhes.push(item.textContent.trim());
+    });
+
+    console.log(JSON.stringify(pacoteTuristico, null, 2));
+}
